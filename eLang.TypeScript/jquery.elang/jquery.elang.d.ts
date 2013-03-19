1ccbc63b4777d5328f04ff157f494e7108b7fe91
sumegizoltan <sumegizoltan73@gmail.com>
2013-03-19 15:29:30 +0100

    TypeScript definition url

--- eLang.TypeScript/jquery.elang/jquery.elang.d.ts
+++ eLang.TypeScript/jquery.elang/jquery.elang.d.ts
@@ -1,7 +1,7 @@
 ﻿// Type definitions for eLang 0.5.1
 // Project: https://github.com/sumegizoltan/ELang/
 // Definitions by: Zoltan Sumegi <https://github.com/sumegizoltan/>
-// Definitions: 
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../jquery/jquery.d.ts" />
 
